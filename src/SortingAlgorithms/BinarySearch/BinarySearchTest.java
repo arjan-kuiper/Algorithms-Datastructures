8281f756649032371fb63470eb34b84c6b73a103
Arjan <arjankuiper@live.nl>
2018-03-29 13:16:15 +0200

    Added binarysearch and binarysearchtest

--- src/SortingAlgorithms/BinarySearch/BinarySearchTest.java
+++ src/SortingAlgorithms/BinarySearch/BinarySearchTest.java
@@ -1,7 +1,17 @@
 package SortingAlgorithms.BinarySearch;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BinarySearchTest {
+    @Test
+    public void search() throws Exception {
+        int[] searchable = new int[]{1, 4, 5, 8, 10, 15, 75, 190};
+        int key = 190;
+        int expectedIndex = 2;
+
+        assertEquals(expectedIndex, BinarySearch.search(searchable, key));
+    }
 
 }
