0b81ccbd6458c89d829128a3951c438ce8e3b5f2
Arjan <arjankuiper@live.nl>
2018-03-29 12:15:04 +0200

    Added InsertionSortTest

--- src/SortingAlgorithms/InsertionSort/InsertionSortTest.java
+++ src/SortingAlgorithms/InsertionSort/InsertionSortTest.java
@@ -1,7 +1,17 @@
 package SortingAlgorithms.InsertionSort;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class InsertionSortTest {
+    @Test
+    public void sort() throws Exception {
+        int toSort[] = new int[]{6, 2, 3, 9, 5};
+        int expected[] = new int[]{2, 3, 5, 6, 9};
+
+        toSort = InsertionSort.sort(toSort);
+        assertArrayEquals(expected, toSort);
+    }
 
 }
