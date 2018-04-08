4b103e601347241672256f38b98a033560d172bc
Alexbijmolt <alex.bijmolt@student.stenden.com>
2018-04-08 14:49:38 +0200

    Queue en test

--- src/Lists/Queue/QueueTest.java
+++ src/Lists/Queue/QueueTest.java
@@ -1,7 +1,62 @@
 package Lists.Queue;
 
+import Lists.LinkedList.LinkedList;
+import Main.Item;
+import Main.JSONHandler;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class QueueTest {
+import java.util.*;
 
+public class QueueTest {
+    private ArrayList<Item> items = new ArrayList<>();
+
+    @Before
+    public void initialize(){
+        items = JSONHandler.getItemsArrayList();
+        System.out.println("All LEGO items were added to an ids array");
+    }
+
+    @Test
+    public void addToQueue(){
+        Queue<Item> queue = new Queue();
+        queue.enqueue(items.get(500));
+        queue.enqueue(items.get(404));
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void removeFromQueue(){
+        Queue<Item> queue = new Queue();
+        queue.enqueue(items.get(500));
+        queue.enqueue(items.get(404));
+        assertEquals(items.get(404) ,queue.dequeue());
+        assertEquals(items.get(500), queue.peek());
+    }
+
+    @Test
+    public void peekQueue(){
+        Queue<Item> queue = new Queue();
+        queue.enqueue(items.get(500));
+        queue.enqueue(items.get(404));
+        assertEquals(items.get(404) ,queue.peek());
+    }
+
+    @Test
+    public void clearQueue(){
+        Queue<Item> queue = new Queue();
+        queue.enqueue(items.get(500));
+        queue.enqueue(items.get(404));
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void checkSizeQueue(){
+        Queue<Item> queue = new Queue();
+        queue.enqueue(items.get(500));
+        queue.enqueue(items.get(404));
+        assertFalse(queue.isEmpty());
+    }
 }
